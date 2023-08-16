cd9f2eb549bf28211ed46c64b774d5c2449064ea
Emma Creteur <emma_creteur@hotmail.com>
2023-08-16 16:22:01 +0200

    rails simple airbnb début model + controller + view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+4.times do
+Flat.create!(
+  name: Faker::Address.unique.building_number,
+  address: Faker::Address.street_address,
+  description: Faker::Lorem.paragraph(sentence_count: 2),
+  price_per_night:  Faker::Number.between(from: 25, to: 800),
+  number_of_guests:  Faker::Number.between(from: 1, to: 10)
+)
+end
